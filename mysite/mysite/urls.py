35896f94f202a18b872a14917f20a7a82cb8e90a
alshafaeeahmed <alshafaeeahmed@gmail.com>
2020-11-30 08:57:48 -0800

    add login \ logout html files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mysite.register import views as v
+from  .import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
