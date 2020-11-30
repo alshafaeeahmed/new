3c7fa9fc87fb34f29df971a2f04cea0e99c7caeb
sondos <sondoal@ac.sce.ac.il>
2020-11-30 16:25:09 +0200

    Adding a class

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-
-class Volunteer(models.Model):
-    #objects = None
-    id = models.UUIDField(primary_key=True)
+class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.CharField(max_length=3)
-    image = models.ImageField(null=True, blank=True, )
+    Id = models.URLField(primary_key=True)
+    age = models.UUIDField(max_length=3)
 
     def __str__(self):
         return self.first_name
@@ -15,24 +12,24 @@ class Volunteer(models.Model):
         return self.last_name
 
 
-class Elderly(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    id = models.UUIDField(primary_key=True)
-    phone_Number = models.CharField(max_length=10)
-    age = models.CharField(max_length=3)
-    image = models.ImageField(null=True, blank=True, )
+class Volunteer(Person):
+        phone_number = models.UUIDField(max_length=10)
 
+class Elderly(Person):
+        phone_number = models.UUIDField(max_length=10)
+        image = models.ImageField(null=True, blank=True, )
 
 class Maneger(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
 
+        first_name = models.CharField(max_length=30)
+        last_name = models.CharField(max_length=30)
+        mail_adress = models.CharField(max_length=100)
 
 class Review(models.Model):
-    rating = models.IntegerField()
-    description = models.TextField()
-    volunteer_id = models.UUIDField()
+        rating = models.IntegerField()
+        description = models.TextField()
+        volunteer_id = models.UUIDField()
 
-    def __str__(self):
+
+def __str__(self):
         return self.description
